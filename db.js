113c608c09b24b8a1fe54eef956e7f32787ba01e
darwin.truong@outlook.com <darwin.truong@outlook.com>
2017-08-26 02:12:04 -0400

    we mongo now boyz

--- db.js
+++ db.js
@@ -1,26 +1,6 @@
-const mysql = require('mysql');
-const config = {
-    host:       'localhost',
-    user:       'user',
-    password:   'pass',
-    database:   'test'
-};
+const db = require('mongoose');
 
-const db = mysql.createConnection(config, (err) => {
-    if (err) {
-        console.error('Error creating connection: ' + err.stack);
-        return;
-    }
-    console.log('Successfully created connection to database');
-});
+db.connect('mongodb://localhost/test');
 
-// Uncomment when REAL database is setup
-// db.connect((err) => {
-//     if (err) {
-//         console.error('Error connecting: ' + err.stack);
-//         return;
-//     }
-//     console.log('Successfully connected to database');
-// });
 
 module.exports = db;
