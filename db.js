61c2d9fbd6295afc57cb82b2da917f922e70cd60
superjohn90 <johnyu1997@hotmail.com>
2017-08-26 02:47:53 -0400

    Connected to the database

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
+const AUTH = require('./auth');
+
 const db = require('mongoose');
 
-db.connect('mongodb://localhost/test');
+db.connect(`mongodb://${AUTH.USER}:${AUTH.PASS}@ds161483.mlab.com:61483/ryerson-smash-db`, { useMongoClient: true } );
 
 
 module.exports = db;
