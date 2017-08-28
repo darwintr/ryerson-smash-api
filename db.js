cd52a7a8f2e619b31da0201a547b818190c048f0
darwin.truong@outlook.com <darwin.truong@outlook.com>
2017-08-28 00:46:35 -0400

    dw

--- db.js
+++ db.js
@@ -1,8 +1,7 @@
 const AUTH = require('./auth');
-
 const db = require('mongoose');
 
-db.connect(`mongodb://${AUTH.USER}:${AUTH.PASS}@ds161483.mlab.com:61483/ryerson-smash-db`, { useMongoClient: true } );
+db.connect(AUTH.DBURL, { useMongoClient: true } );
 
 
 module.exports = db;
