876dfa3cab235adc912a85bc8f5e41fa2bd7593a
superjohn90 <johnyu1997@hotmail.com>
2017-08-29 00:44:54 -0400

    Added character and stage constants

---
+++ api/constants/Characters.js
@@ -0,0 +1,8 @@
+exports.CHARACTERS = { 
+						drmario: 'Dr.Mario', mario: 'Mario', luigi: 'Luigi', bowser: 'Bowser', peach: 'Peach',
+						yoshi: 'Yoshi', dk: 'Donkey Kong', captainfalcon: 'Captain Falcon', ganondorf: 'Ganondorf',
+						falco: 'Falco', fox: 'Fox', ness: 'Ness', iceclimbers: 'Ice Climbers', kirby: 'Kirby',
+						samus: 'Samus', zelda: 'Zelda', sheik: 'Sheik', link: 'Link', younglink: 'Young Link',
+						pichu: 'Pichu', pikachu: 'Pikachu', jigglypuff: 'Jigglypuff', mewtwo: 'Mewtwo', 
+						gnw: 'Mr. Game & Watch', marth: 'Marth', roy: 'Roy', 
+					}
---
+++ api/constants/Stages.js
@@ -0,0 +1,4 @@
+exports.STAGES = {
+					battlefield: 'Battlefield', dreamland: 'Dream Land 64' , fd: 'Final Destination',
+					yoshistory: 'Yoshi\'s Story', pokemonstadium: 'Pokemon Stadium', other: 'Other'
+}
