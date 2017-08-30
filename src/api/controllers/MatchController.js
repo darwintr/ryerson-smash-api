cef2ecb49b72d7ff925d615f4e0accdf6859cbfc
KevinMKang <kevinmkang97@gmail.com>
2017-08-30 17:49:16 -0400

    added validating match schema and controller

--- src/api/controllers/MatchController.js
+++ src/api/controllers/MatchController.js
@@ -1,10 +1,75 @@
 import express from 'express';
 import db from '../../db';
-import MatchSchema from '../models/Match';
+import Models from '../models/ModelIndex';
 import * as charas from '../constants/Characters';
 import * as stages from '../constants/Stages'
 import bodyParser from 'body-parser';
 
 let router = express.Router();
-//router.use(bodyParser.urlencoded( { extended: true } ));
-let Match = db.model('Match', MatchSchema);
+router.use(bodyParser.urlencoded( { extended: true } ));
+router.use(bodyParser.json());
+let Match = db.model('Match', Models.MatchModel);
+
+router.get('/', (req, res) => {
+
+    Match.find()
+        .then((p, e) => {
+            if(e){
+                throw 'Database error';
+            } else{
+                res.status(200).send(p)
+            }
+        })
+        .catch((e) => {
+            res.status(500).send(e);
+        });
+});
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    if(!req.body.players || !req.body.stage){
+        return res.status(400).send('Bad Request');
+    }
+
+    let newMatch =  new Match({
+        players: req.body.players,
+        winners: req.body.winners,
+        stage: req.body.stage
+    });
+
+
+
+    newMatch.save()
+        .then((p, e) => {
+            if (e) {
+                throw 'Database error';
+            } else {
+                res.status(200).send('Successfully added Match');
+                //console.log(p);
+            }
+        }, function(err){
+            //console.log(err);
+            res.status(400).send(err.message.replace(', ', "\n"));
+        });
+});
+
+router.put('/', (req, res) => {
+
+});
+
+router.delete('/', (req, res) => {
+    if(!req.query.id){
+        return res.status(400).send("Bad Request");
+    }
+
+    Match.findByIdAndRemove({_id : req.query.id})
+        .then((p, e) =>{
+            if(e){
+                res.status(400).send("Failed to delete");
+            }else{
+                res.status(200).send("Match deleted");
+            }
+        });
+});
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded( { extended: true } ));
 app.use(bodyParser.json());
 // ROUTES
 app.use('/player', Controllers.PlayerController);
+app.use('/match', Controllers.MatchController);
 
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}.`);
