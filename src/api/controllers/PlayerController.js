e268b241783f2599f1c393e0a9b5fc3fe0507d8d
darwin.truong@outlook.com <darwin.truong@outlook.com>
2017-08-31 02:24:36 -0400

    add update and delete route, cleaned up some code in player route

--- src/api/controllers/PlayerController.js
+++ src/api/controllers/PlayerController.js
@@ -8,16 +8,32 @@ router.use(bodyParser.urlencoded( { extended: true } ));
 router.use(bodyParser.json());
 let Player = db.model('Player', Models.PlayerModel);
 
-// TODO: Move exceptions to a separate file/implementation
+// TODO: Move exceptions to a separate file/implementation, fix the ugly exception implementation
+// TODO: Document this somewhere (probably in a md file)
 
 const handleErr = (e, res) => {
     switch (e) {
         case 'Player exists error':
             res.status(400).send('Bad Request: Player already exists');
             break;
+        case 'Player not found error':
+            res.status(204).send('Bad Request: Player not found');
+            break;
         default:
             res.status(500).send(e.message ? e.message : e);
     }
+};
+
+// Builds the query dynamically
+const playerQueryBuilder = (tag, name) => {
+    let query = {};
+    if (tag) {
+        query.tag = tag
+    }
+    if (name) {
+        query.name = name
+    }
+    return query
 };
 
 router.get('/', (req, res) => {
@@ -35,32 +51,47 @@ router.get('/', (req, res) => {
         }
     };
 
-    // Find all if no tag and name are inputted
-    // Otherwise find document with tag and name
-    if (!req.query.tag && !req.query.name) {
-        Player.find()
-            .then((p, e) => handleGet(p, e))
-            .catch((e) => handleErr(e, res));
-    } else {
-        Player.find({ name: req.query.name, tag: req.query.tag })
-            .then((p, e) => handleGet(p, e))
-            .catch((e) => handleErr(e, res));
-    }
+    Player.find(playerQueryBuilder(req.query.tag, req.query.name))
+        .then((p, e) => handleGet(p, e))
+        .catch((e) => handleErr(e, res));
 
 
 });
 
 router.put('/', (req, res) => {
     // UPDATE A PLAYER
-    // PARAMS: tag and/or name
+    // PARAMS: tag and name
 
-    // TODO: update route
+    // handler for request; called after query
+    const handleUpdate  = (p, e) => {
+        if (e) {
+            throw e;
+        } else if (!p) {
+            throw 'Player not found';
+        } else {
+            res.status(200).send(`Successfully updated Player ${p.name}:${p.tag} to `);
+        }
+    };
 
-    console.log(req.body);
-    let qName = req.body.name,
-        qTag = req.body.tag,
-        qMain = req.body.main;
+    // Build the update object dynamically
+    const buildUpdate = (query) => {
+        let updateObject = {},
+            fields = ['ntag', 'nname', 'nmain'];
 
+        for (let key in query) {
+            if (query.hasOwnProperty(key) && fields.includes(key) ) {
+                updateObject[key.substring(1)] = query[key];
+            }
+        }
+        return updateObject;
+    };
+
+    if (req.query.tag && req.query.name) {
+        Player.findOneAndUpdate(playerQueryBuilder(req.query.tag, req.query.name),
+            {$set: buildUpdate(req.query)})
+            .then((p, e) => handleUpdate(p, e))
+            .catch((e) => handleErr(e, res));
+    }
 
 });
 
@@ -71,48 +102,47 @@ router.post('/', (req, res) => {
     console.log(req.body);
     let qName = req.body.name,
         qTag = req.body.tag,
-        qMain = req.body.main.toLowerCase();
+        qMain = req.body.main;
 
-    // Check if player already exists, if it doesn't then add the player
-    Player.find({ name: qName, tag: qTag })
+    let newPlayer = new Player({
+        name: qName,
+        tag: qTag,
+        main: qMain});
+
+    newPlayer.save()
         .then((p, e) => {
-            return new Promise((resolve) => {
-                    if (e) {
-                        throw 'Database error';
-                    }
-                    if (p.length) {
-                        console.log(p);
-                        throw 'Player exists error'
-                    }
-                    resolve();
-                }
-            )
-        })
-        .then(() => {
-            let newPlayer = new Player({
-                name: qName,
-                tag: qTag,
-                main: qMain});
-
-            newPlayer.save()
-                .then((p, e) => {
-                    if (e) {
-                        throw 'Database error';
-                    } else {
-                        res.status(200).send('Successfully added Player');
-                        console.log(p);
-                    }
-                })
-                .catch((e) => handleErr(e, res));;
+            if (e) {
+                throw 'Database error';
+            } else {
+                res.status(200).send(`Successfully added Player ${qName}:${qTag}`);
+                console.log(p);
+            }
         })
         .catch((e) => handleErr(e, res));
 });
 
 router.delete('/', (req, res) => {
     // DELETE A PLAYER
-    // PARAMS: tag and/or name ; id else: res: 400 / res.message = "more than one player with that tag, get id"
+    // PARAMS: tag and name ; id else: res: 400 / res.message = "more than one player with that tag, get id"
 
-    // TODO: delete route
+    // handler for request; called after query
+    const handleDelete = (p, e) => {
+        if (e) {
+            throw e;
+        } else if (!p) {
+            throw 'Player not found';
+        } else {
+            res.status(200).send(`Successfully deleted Player ${p.name}:${p.tag}`);
+        }
+    };
+
+    if (req.query.tag && req.query.name) {
+        Player.findOneAndRemove(playerQueryBuilder(req.query.tag, req.query.name))
+            .then((p, e) => handleDelete(p, e))
+            .catch((e) => handleErr(e, res));
+    }
+
+
 });
 
 module.exports = router;
